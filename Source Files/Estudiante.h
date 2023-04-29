b2f75f9293090c72c766c94ff5515ead7ccd85ad
Mathiashv18 <mathiashv2014@gmail.com>
2023-04-29 15:30:13 -0600

    Set & get clase Persona

--- Source Files/Estudiante.h
+++ Source Files/Estudiante.h
@@ -11,8 +11,18 @@ public:
     this -> nombre = nombre;
     this -> apellido = apellido;
     this -> edad = edad;
-
+    this -> tipoDocumento = tipoDocumento;
     };
     ~Estudiante(){};
+
+    void setNombre(string nombre){this->nombre = nombre;}
+    void setApellido(string apellido){this->apellido = apellido;}
+    void setEdad(int edad){this->edad = edad;}
+    void setTipoDocumento(char tipoDocumento){this->tipoDocumento = tipoDocumento;}
+
+    string getNombre(){return this->nombre;}
+    string getApellido(){return this->apellido;}
+    int getEdad(){return this->edad;}
+    char getTipoDocumento(){return this->tipoDocumento;}
 };
 
