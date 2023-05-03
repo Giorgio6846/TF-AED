42fca5ecbece6b4368647562a262337dc6972648
MathiasHv18 <mathiashv2014@gmail.com>
2023-05-02 22:04:42 -0500

    final encomienda menu

---
+++ SourceFiles/Encomienda.h
@@ -0,0 +1,35 @@
+#include "Libraries.h"
+
+class Encomienda
+{
+private:
+    /* data */
+public:
+    Encomienda(/* args */){
+
+
+
+    };
+    ~Encomienda(){};
+
+
+    int menuEncomiendas(){
+
+    int opcionSelecionada;
+    do
+    {
+        cout << "Selecione la opcion. \n";
+        cout << "1. Agendar una encomienda \n";
+        cout << "2. Eliminar una encomienda \n";
+        cout << "3. Regresar al menu principal \n";
+
+        cin >> opcionSelecionada;
+
+        if (!(opcionSelecionada >= 1 && opcionSelecionada <= 4))
+            cout << "La opcion seleccionada es incorrecta.\n";
+        else return opcionSelecionada;
+    } while (opcionSelecionada != 3);
+
+    return opcionSelecionada;
+    }
+};
