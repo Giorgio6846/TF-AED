fbdfd43cbc45dc0375010a98fa5c83c7c9f9dde4
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2023-05-02 21:17:01 -0500

    Minor changes

---
+++ SourceFiles/Horarios.h
@@ -0,0 +1,18 @@
+#include "Libraries.h"
+
+class Horarios
+{
+private:
+    /* data */
+public:
+    Horarios(/* args */);
+    ~Horarios();
+};
+
+Horarios::Horarios(/* args */)
+{
+}
+
+Horarios::~Horarios()
+{
+}
---
+++ SourceFiles/informacionRuta.h
@@ -0,0 +1,18 @@
+#include "Libraries.h"
+
+class informacionRuta
+{
+private:
+    /* data */
+public:
+    informacionRuta(/* args */);
+    ~informacionRuta();
+};
+
+informacionRuta::informacionRuta(/* args */)
+{
+}
+
+informacionRuta::~informacionRuta()
+{
+}
