dced344fd606a18ba21617e4e7e76150ab31aaf2
Giorgio6846 <6852733+Giorgio6846@users.noreply.github.com>
2023-06-14 23:40:08 -0500

    Minor changes

---
+++ SourceFiles/Algoritmos/minHeap.h
@@ -0,0 +1,23 @@
+#pragma once
+ 
+#include "NodoLista.h"
+#include "../Libraries.h"
+
+
+
+class minHeap
+{
+private:
+
+public:
+    minHeap();
+    ~minHeap();
+};
+
+minHeap::minHeap()
+{
+}
+
+minHeap::~minHeap()
+{
+}
