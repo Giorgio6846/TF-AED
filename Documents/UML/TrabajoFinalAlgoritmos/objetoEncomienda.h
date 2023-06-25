24b5e62485a2f3c29b98ac6cb84950547add1807
MathiasHv18 <65747481+MathiasHv18@users.noreply.github.com>
2023-06-25 11:22:26 -0600

    dsadsad

---
+++ Documents/UML/TrabajoFinalAlgoritmos/objetoEncomienda.h
@@ -0,0 +1,48 @@
+#pragma once
+#include "Libraries.h"
+
+class objetoEncomienda
+{
+private:
+    string categoria;
+    float peso;
+    bool esFragil;
+    string fragil;
+public:
+    //Constructor para input manual
+    objetoEncomienda(string categoria, float peso, int esFragil) {
+        this->peso = peso;
+        this->esFragil = esFragil;
+        this->categoria = categoria;
+    };
+    //Constructor para DataSet
+    objetoEncomienda() {
+        this->peso = rand() % 100;
+        this->esFragil = rand() % 2;
+        this->categoria = "Tecnologia";
+    };
+    ~objetoEncomienda() {};
+
+    void setCategoria(string categoria) { this->categoria = categoria; }
+    void setPeso(float peso) { this->peso = peso; }
+    void setEsFragilI(int esFragil) { this->esFragil = esFragil; }
+
+    bool getEsFragil() { return this->esFragil; }
+    string getCategoria() { return this->categoria; }
+    float getPeso() { return this->peso; }
+
+    void infoEncomienda() {
+        cout << "Informacion del objeto: " << endl;
+        cout << "Peso: " << peso << "Kg" << endl;
+        if (esFragil == true)
+        {
+            fragil = "Si";
+        }
+        else
+        {
+            fragil = "No";
+        }
+        cout << "Es fragil?: " << fragil << endl;
+    }
+
+};
